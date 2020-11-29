75a49126158cccb5c7d149bba20a30434597c038
Tibing <nik.poltoratsky@gmail.com>
2020-11-29 17:12:15 +0300

    feat(plugin): add more examples

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
 export default function ({ types: t }) {
   return {
     visitor: {
-      DebuggerStatement(path) {
-        path.remove();
-      },
 
-      CallExpression(path) {
-        if (path.node.callee.object.name === 'console') {
-          path.remove();
-        }
-      },
+      // Remove debugger statements
+      // DebuggerStatement(path) {
+      //   path.remove();
+      // },
+
+      // Remove debuggers inside if statements
+      // DebuggerStatement(path) {
+      //   if (t.isIfStatement(path.parentPath.parentPath)) {
+      //     path.remove();
+      //   }
+      // },
+
+      // Replace debuggers with console logs
+      // DebuggerStatement(path) {
+      //   const consoleLog = t.callExpression(
+      //     t.memberExpression(
+      //       t.identifier('console'),
+      //       t.identifier('log'),
+      //     ),
+      //     [t.stringLiteral('Debugger was here!!!')],
+      //   );
+      //
+      //   path.insertBefore(consoleLog);
+      //   path.remove();
+      // },
+
+      // Remove console logs
+      // CallExpression(path) {
+      //   if (path.node.callee.object.name === 'console') {
+      //     path.remove();
+      //   }
+      // },
     },
   }
 }
